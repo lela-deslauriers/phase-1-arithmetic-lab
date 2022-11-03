6877d7ffb891b824348b4f4aabc0f7818dfab974
Lela Deslauriers <lela.deslauriers@gmail.com>
2022-11-02 17:43:47 -0700

    commit tree 9aa5a979be1f8de339ff18c996591f8cf6ff9771

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+const num1 = 2;
+const num2 = 31;
 
-// Write your code here
+let multiply = num1 * num2; // is an equation whose multiplied results will equal 62
+
+
+let random = Math.ceil(Math.random()); // generates a random integer greater than 0
+
+
+const num3 = 19;
+const num4 = 5;
+
+let mod = num3 % num4; // is an equation that calculates a remainder that is equal to 4
+
+
+let max =  Math.max(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 20); // will return 20 as the highest number in the set
+
